5282083eb42299cfad12855f72243df486ebfcfb
jpimenteljose <jpimentel.jose@hotmail.com>
2023-10-23 16:52:40 -0300

    Fet: Desenvolvimento do mapeamento do banco de dados.

---
+++ ProdutosApp.Data/Migrations/20231023194528_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProdutosApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PRODUTO",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NOME = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    PRECO = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QUANTIDADE = table.Column<int>(type: "int", nullable: false),
+                    DATAHORACRIACAO = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DATAHORAULTIMACRIACAO = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUTO", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PRODUTO");
+        }
+    }
+}
