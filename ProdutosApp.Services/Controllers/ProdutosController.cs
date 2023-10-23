f427ecbb72a7c5e601904820eaabd4a4a1a9fb45
jpimenteljose <jpimentel.jose@hotmail.com>
2023-10-23 18:14:14 -0300

    Feat: Desenvolvimento da inclusão de Produtos.

--- ProdutosApp.Services/Controllers/ProdutosController.cs
+++ ProdutosApp.Services/Controllers/ProdutosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProdutosApp.Data.Entities;
+using ProdutosApp.Data.Repositories;
 using ProdutosApp.Services.Models;
 
 namespace ProdutosApp.Services.Controllers
@@ -16,7 +18,34 @@ namespace ProdutosApp.Services.Controllers
         [ProducesResponseType(typeof(ProdutosGetModel), 200)]
         public IActionResult Post([FromBody] ProdutosPostModel model)
         {
-            return Ok();
+            try
+            {
+                // criando um objeto da classe Produto
+                var produto = new Produto
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = model.Nome,
+                    Preco = model.Preco,
+                    Quantidade = model.Quantidade,
+                    DataHoraCriacao = DateTime.Now,
+                    DataHoraUltimaAlteracao = DateTime.Now
+                };
+
+                // gravar no banco de dados
+                var produtoRepository = new ProdutoRepository();
+                produtoRepository.Add(produto);
+
+                // copiando os dados do produto para a classe ProdutosGetModel
+                var result = EntityToModel(produto);
+
+                // retornar 200 (ok)
+                return StatusCode(200, result);
+            }
+            catch(Exception e)
+            {
+                // retornar erro 500 (INTERNAL SERVER ERROR)
+                return StatusCode(500, new { e.Message });
+            }
         }
 
         /// <summary>
@@ -40,6 +69,7 @@ namespace ProdutosApp.Services.Controllers
         {
             return Ok();
         }
+        
         /// <summary>
         /// Método para serviço de consulta de produtos:
         /// GET /api/produtos
@@ -51,11 +81,35 @@ namespace ProdutosApp.Services.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Método para serviço de consulta de UM produto por ID:
+        /// GET /api/produtos
+        /// </summary>
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(ProdutosGetModel), 200)]
         public IActionResult GetById(Guid? id)
         {
             return Ok();
+        }
+    
+        /// <summary>
+        /// Método auxiliar para copiar os dados de um objeto da classe Produto para um objeto da classe ProdutosGetModel
+        /// </summary>
+        /// <param name="produto"></param>
+        /// <returns></returns>
+        private ProdutosGetModel EntityToModel(Produto produto)
+        {
+            // copiando os dados do Produto para a classe ProdutoGetModel
+            return new ProdutosGetModel
+            {
+                Id = produto.Id,
+                Nome = produto.Nome,
+                Preco = produto.Preco,
+                Quantidade = produto.Quantidade,
+                Total = produto.Preco * produto.Quantidade,
+                DataHoraCriacao = produto.DataHoraCriacao,
+                DataHoraUltimaAlteracao = produto.DataHoraUltimaAlteracao
+            };
         }
     }
 }
--- ProdutosApp.Services/Models/ProdutosGetModel.cs
+++ ProdutosApp.Services/Models/ProdutosGetModel.cs
@@ -12,6 +12,7 @@ namespace ProdutosApp.Services.Models
         public string? Nome { get; set; }
         public decimal? Preco { get; set; }
         public int? Quantidade { get; set; }
+        public decimal? Total { get; set; }
         public DateTime? DataHoraCriacao { get; set; }
         public DateTime? DataHoraUltimaAlteracao { get; set; }
     }
